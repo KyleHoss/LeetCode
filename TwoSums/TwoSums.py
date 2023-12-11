26bfd8ea591d55631256272a43b9b81fcf25386b
Kyle Hostetter <Kyle.Hostetter13@gmail.com>
2023-12-11 11:36:27 -0600

    Finished the updates for this thing!

--- TwoSums/TwoSums.py
+++ TwoSums/TwoSums.py
@@ -1,5 +1,25 @@
 class Solution:
-    def twoSum(self, nums, target):
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        prevMap = {}
 
-        # We are just going ot check this thing out first. 
-        
+        for i, n in enumerate(nums):
+            diff = target - n
+            print('Difference Print:',diff)
+            if diff in prevMap:
+                print('If return print:',prevMap[diff], i)
+                return [prevMap[diff], i]
+            prevMap[n] = i
+            print('After if statement print:',prevMap)
+        return 
+    
+# Solution.twoSum(0,[2,7,11,15], 9)
+
+
+def twoy(nums: list[int], target: int) -> list[int]:
+    for i in range(0,len(nums)):
+        for j in range(0,len(nums)):
+            if nums[i] + nums[j] == target:
+                print([i,j])
+                return [i, j]
+            
+twoy([5,2,11,15], 17)
