bf1a152e5bd1dcb824659a9be42b81b4489baa84
Kyle Hostetter <Kyle.Hostetter13@gmail.com>
2023-12-17 17:51:07 -0600

    This is just the full problem.

--- RomanToInteger/RomanToInteger.py
+++ RomanToInteger/RomanToInteger.py
@@ -12,4 +12,19 @@ def romanToInt(s: str) -> int:
             sum = sum + num[i]
     print(sum)
 
-romanToInt("MDLVIII")
+romanToInt("LVIII")
+
+
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        sum=0
+        num = [0] * (len(s)+1)
+        for i in range(len(s)-1,-1,-1):
+            num[i] = roman[s[i]]
+            if  num[i+1] > num[i]:
+                sum = sum - num[i]
+            else:
+                sum = sum + num[i]
+        return sum
