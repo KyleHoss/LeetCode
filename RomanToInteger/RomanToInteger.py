002940ea171927e49af1cf84e1b86700a41d1493
Kyle Hostetter <Kyle.Hostetter13@gmail.com>
2023-12-17 17:50:52 -0600

    This is the Roman to Integer problem.

---
+++ RomanToInteger/RomanToInteger.py
@@ -0,0 +1,15 @@
+
+def romanToInt(s: str) -> int:
+    
+    roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+    sum=0
+    num = [0] * (len(s)+1)
+    for i in range(len(s)-1,-1,-1):
+        num[i] = roman[s[i]]
+        if  num[i+1] > num[i]:
+            sum = sum - num[i]
+        else:
+            sum = sum + num[i]
+    print(sum)
+
+romanToInt("MDLVIII")
